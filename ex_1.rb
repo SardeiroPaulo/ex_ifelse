a3b7795ac47ed40c4f3de4035cc7488de73cc392
SardeiroPaulo <131005207+SardeiroPaulo@users.noreply.github.com>
2024-03-07 10:56:44 -0300

    lista1

---
+++ ex_1.rb
@@ -0,0 +1,11 @@
+#1 – LEIA UM NÚMERO E MOSTRE SE ELE É IGUAL A 20
+
+puts "Digite um número: "
+n = gets.chomp.to_i
+
+if ( n == 20 ) then
+    puts "O número é igual a 20"
+else
+    puts "Número diferente de 20"
+end
+    
